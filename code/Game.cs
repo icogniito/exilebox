9e501f80e3ba9dcc2f76517b72ef98d07a8265f9
icogniito <jacob.hannerfors@gmail.com>
2023-04-08 14:17:42 +0200

    Basic structure and mechanics

--- code/Game.cs
+++ code/Game.cs
@@ -1,14 +1,12 @@
-﻿using Sandbox;
-using Sandbox.UI.Construct;
+﻿
+using Sandbox;
 using System;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 
 //
 // You don't need to put things in a namespace, but it doesn't hurt.
 //
-namespace Sandbox;
+namespace ExileBox;
 
 /// <summary>
 /// This is your game class. This is an entity that is created serverside when
@@ -17,17 +15,14 @@ namespace Sandbox;
 /// You can use this to create things like HUDs and declare which player class
 /// to use for spawned players.
 /// </summary>
-public partial class ExileBoxGame : GameManager
+public partial class MyGame : Sandbox.GameManager
 {
-	public ExileBoxGame()
+	public MyGame()
 	{
-
 		if ( Game.IsClient )
 		{
-			//create the HUD
-			_ = new Hud();
+			Game.RootPanel = new Hud();
 		}
-
 	}
 
 	/// <summary>
@@ -40,7 +35,8 @@ public partial class ExileBoxGame : GameManager
 		// Create a pawn for this client to play with
 		var pawn = new Pawn();
 		client.Pawn = pawn;
-
+		pawn.Respawn();
+		pawn.DressFromClient( client );
 
 		// Get all of the spawnpoints
 		var spawnpoints = Entity.All.OfType<SpawnPoint>();
@@ -57,3 +53,4 @@ public partial class ExileBoxGame : GameManager
 		}
 	}
 }
+
---
+++ code/pawn/PawnCamera.cs
@@ -0,0 +1,95 @@
+using Sandbox;
+using System;
+using System.Linq;
+
+namespace ExileBox;
+
+public partial class PawnCamera : EntityComponent<Pawn>, ISingletonComponent
+{
+	protected float WheelSpeed => 30f;
+	protected Vector2 CameraDistance => new( 125, 1000 );
+	protected Vector2 PitchClamp => new( 30, 60 );
+
+	public object Main { get; internal set; }
+
+	float OrbitDistance = 400f;
+	float TargetOrbitDistance = 400f;
+	Angles OrbitAngles = Angles.Zero;
+
+	protected static Vector3 IntersectPlane( Vector3 pos, Vector3 dir, float z )
+	{
+		float a = (z - pos.z) / dir.z;
+		return new( dir.x * a + pos.x, dir.y * a + pos.y, z );
+	}
+
+	protected static Rotation LookAt( Vector3 targetPosition, Vector3 position )
+	{
+		var targetDelta = (targetPosition - position);
+		var direction = targetDelta.Normal;
+
+		return Rotation.From( new Angles(
+			((float)Math.Asin( direction.z )).RadianToDegree() * -1.0f,
+			((float)Math.Atan2( direction.y, direction.x )).RadianToDegree(),
+			0.0f ) );
+	}
+
+	public void Update()
+	{
+		var pawn = Entity;
+		if ( !pawn.IsValid() )
+			return;
+
+		Camera.Position = pawn.Position;
+		Vector3 targetPos;
+
+		Camera.Position += Vector3.Up * (pawn.CollisionBounds.Center.z * pawn.Scale);
+		Camera.Rotation = Rotation.From( OrbitAngles );
+
+		targetPos = Camera.Position + Camera.Rotation.Backward * OrbitDistance;
+
+		Camera.Position = targetPos;
+		Camera.FieldOfView = 70f;
+		Camera.FirstPersonViewer = null;
+		Camera.Main.Ortho = true;
+		Camera.Main.OrthoHeight = 600;
+		Camera.Main.OrthoWidth = 800;
+
+		Sound.Listener = new()
+		{
+			Position = pawn.AimRay.Position,
+			Rotation = pawn.EyeRotation
+		};
+	}
+
+	public void BuildInput()
+	{
+		var wheel = -50;
+		if ( wheel != 0 )
+		{
+			TargetOrbitDistance -= wheel * WheelSpeed;
+			TargetOrbitDistance = TargetOrbitDistance.Clamp( CameraDistance.x, CameraDistance.y );
+		}
+
+		OrbitDistance = OrbitDistance.LerpTo( TargetOrbitDistance, Time.Delta * 10f );
+
+		if ( Input.UsingController || Input.Down( InputButton.SecondaryAttack ) )
+		{
+			OrbitAngles.yaw += Input.AnalogLook.yaw;
+			OrbitAngles.pitch += Input.AnalogLook.pitch;
+			OrbitAngles = OrbitAngles.Normal;
+
+			Entity.ViewAngles = OrbitAngles.WithPitch( 0f );
+		}
+		else
+		{
+			var direction = Screen.GetDirection( Mouse.Position, Camera.FieldOfView, Camera.Rotation.Normal, Screen.Size );
+			var hitPos = IntersectPlane( Camera.Position, direction, Entity.EyePosition.z );
+
+			Entity.ViewAngles = (hitPos - Entity.EyePosition).EulerAngles;
+		}
+
+		OrbitAngles.pitch = OrbitAngles.pitch.Clamp( PitchClamp.x, PitchClamp.y );
+
+		Entity.InputDirection = Input.AnalogMove;
+	}
+}
