f9b5b8ac310f2a01f619e5690813cfd43c811ba7
icogniito <jacob.hannerfors@gmail.com>
2023-04-08 02:23:48 +0200

    initial commit

---
+++ code/Player.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+
+namespace Sandbox;
+
+partial class Pawn : AnimatedEntity
+{
+	/// <summary>
+	/// Called when the entity is first created 
+	/// </summary>
+	/// 
+	public ClothingContainer Clothing = new();
+
+	/// <summary>
+	/// Default init
+	/// </summary>
+	public Pawn()
+	{
+	}
+
+	/// <summary>
+	/// Initialize using this client
+	/// </summary>
+	public Pawn( IClient cl ) : this()
+	{
+		// Load clothing from client data
+		Clothing.LoadFromClient( cl );
+	}
+
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		//
+		// Use a citizen model
+		//
+		SetModel( "models/citizen/citizen.vmdl" );
+
+		EnableDrawing = true;
+		EnableHideInFirstPerson = false;
+		EnableShadowInFirstPerson = true;
+
+		Health = 100;
+	}
+
+
+	// An example BuildInput method within a player's Pawn class.
+	[ClientInput] public Vector3 InputDirection { get; protected set; }
+	[ClientInput] public Angles ViewAngles { get; set; }
+
+	public override void BuildInput()
+	{
+		InputDirection = Input.AnalogMove;
+
+		var look = Input.AnalogLook;
+
+		var viewAngles = ViewAngles;
+		viewAngles += look;
+		ViewAngles = viewAngles.Normal;
+	}
+
+	
+	/// <summary>
+	/// Called every tick, clientside and serverside.
+	/// </summary>
+	public override void Simulate( IClient cl )
+	{
+		base.Simulate( cl );
+
+		Rotation = ViewAngles.ToRotation();
+
+		// build movement from the input values
+		var movement = InputDirection.Normal;
+
+		// rotate it to the direction we're facing
+		Velocity = Rotation * movement;
+
+		// apply some speed to it
+		Velocity *= Input.Down( InputButton.Run ) ? 1000 : 200;
+
+		// apply it to our position using MoveHelper, which handles collision
+		// detection and sliding across surfaces for us
+		MoveHelper helper = new MoveHelper( Position, Velocity );
+		helper.Trace = helper.Trace.Size( 16 );
+		if ( helper.TryMove( Time.Delta ) > 0 )
+		{
+			Position = helper.Position;
+		}
+
+		// If we're running serverside and Attack1 was just pressed, spawn a ragdoll
+		if ( Game.IsServer && Input.Pressed( InputButton.PrimaryAttack ) )
+		{
+			var ragdoll = new ModelEntity();
+			ragdoll.SetModel( "models/citizen/citizen.vmdl" );
+			ragdoll.Position = Position + Rotation.Forward * 40;
+			ragdoll.Rotation = Rotation.LookAt( Vector3.Random.Normal );
+			ragdoll.SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
+			ragdoll.PhysicsGroup.Velocity = Rotation.Forward * 1000;
+
+			Health -= 10;
+		}
+	}
+
+	/// <summary>
+	/// Called every frame on the client
+	/// </summary>
+	public override void FrameSimulate( IClient cl )
+	{
+		base.FrameSimulate( cl );
+
+		Camera.Position = Position + Vector3.Up * 400f + Vector3.Backward * 300f + Vector3.Right * 300f;
+		Camera.Rotation = Rotation.FromYaw( 45f ) * Rotation.FromPitch( 40f );
+		Camera.Main.Ortho = true;
+		Camera.Main.OrthoHeight = 600;
+		Camera.Main.OrthoWidth = 800;
+	}
+}
